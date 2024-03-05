e8083138c14ce095f9777e94e4efc0f70a43681b
taichi221228 <taichi221228@icloud.com>
2024-03-05 21:27:21 +0900

    feat: Add post route and update HTML response
    This commit adds a new post route '/submit' to the application in our index.ts file. It also optimizes the homepage route by storing the index.html content in a variable. In addition, the application's index.html is updated with a form, changing the main view from a 'Hello World!' greeting to a user-input form.

--- index.ts
+++ index.ts
@@ -3,6 +3,13 @@ import fs from "fs";
 
 const app = new Hono();
 
-app.get("/", (c) => c.html(fs.readFileSync("index.html", "utf8")));
+const html = fs.readFileSync("index.html", "utf8");
+
+app.get("/", (c) => c.html(html));
+
+app.post("/submit", async (c) => {
+	const { name } = await c.req.parseBody();
+	return c.html(name.toString());
+});
 
 export default app;
